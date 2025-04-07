744a50d092eb752cf827eaaee134b7dc6f854087
DANIS <danis.preldzic@gmail.com>
2025-04-07 18:45:23 +0200

    adding factory package, changing Package to include factory dep

--- Package.swift
+++ Package.swift
@@ -1,24 +1,31 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SPM_Foundations",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SPM_Foundations",
             targets: ["SPM_Foundations"]),
     ],
+    dependencies: [
+        // Factory dependency
+        .package(url: "https://github.com/hmlongco/Factory", from: "2.3.1"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SPM_Foundations"),
+            name: "SPM_Foundations",
+            dependencies: [
+                // ADD: Factory to target dependencies
+                .product(name: "Factory", package: "Factory"),
+            ]),
         .testTarget(
             name: "SPM_FoundationsTests",
-            dependencies: ["SPM_Foundations"]
-        ),
+            dependencies: ["SPM_Foundations"]),
     ]
 )
--- Sources/SPM_Foundations/SPM_Foundations.swift
+++ Sources/SPM_Foundations/SPM_Foundations.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import Factory
+
+
+final public class Foundations: SharedContainer {
+    public let manager = ContainerManager()
+    
+    public static let shared = Foundations()
+}
+
+extension Foundations {
+    
+    var storageManager: Factory<StorageManager> {
+        self { StorageManagerImplementation.shared }
+            .singleton
+    }
+}
